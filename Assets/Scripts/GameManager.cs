2492994e7392a1c261b99a2217784cd0b6e7806d
TheExkaiser <omartinez@int.pl>
2025-04-08 21:39:41 +0200

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using DG.Tweening;
 
 
 public class GameManager : MonoBehaviour
@@ -11,10 +12,13 @@ public class GameManager : MonoBehaviour
     public Transform cardSlotsPool;
     public Player player;
     public SpecialAbilities specialAbilities;
+    public Transform hqDeck;
+    public List<CardSO> hqDeckList;
 
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -33,6 +37,24 @@ public class GameManager : MonoBehaviour
             int r = Random.Range(t, deck.Count);
             deck[t] = deck[r];
             deck[r] = tmp;
+        }
+    }
+
+    public void AddCardToHQ(Transform slot) 
+    { 
+        if (hqDeckList.Count > 0) 
+        {
+            Transform card = cardsPool.GetChild(0);
+            RectTransform cardRect = card.GetComponent<RectTransform>();
+            Card cardScript = card.gameObject.GetComponent<Card>();
+
+            cardRect.localPosition = hqDeck.GetComponent<RectTransform>().localPosition;
+            card.SetParent(slot);
+            cardScript.cardData = hqDeckList[0];
+            cardScript.PopulateCardPrefab();
+            card.gameObject.SetActive(true);
+            cardRect.DOAnchorPos(slot.localPosition, 0.5f);
+            
         }
     }
 
